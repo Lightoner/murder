5ecefcc0800ce2ed65c4cbb0064694b61aacba6c
Lightoner <lightonergaming@gmail.com>
2018-04-22 13:53:22 +0200

    Fix respawn in spectator

--- gamemode/sv_respawn.lua
+++ gamemode/sv_respawn.lua
@@ -2,6 +2,7 @@
 
 function GM:CanRespawn(ply)
 	if self:GetRound() == 0 then
+		if ply:Team() ~= 2 then return end
 		if ply.NextSpawnTime && ply.NextSpawnTime > CurTime() then return end
 		
 		if ply:KeyPressed(IN_JUMP) || ply:KeyPressed(IN_ATTACK) then
